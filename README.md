ed1a7f24073f3930cbda8fcd17e57b1bfc1616f4
Wirt4 <wirt.salthouse@gmail.com>
2021-05-12 17:45:04 -0700

    program functioning, will re-factor

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # memory-game
 JS -based memory game taken from the tutorial at https://www.youtube.com/watch?v=lhNdUVh3qCc 
 
-Step 1: Gather assets
+can unflip the cards with event listeners, but having diffuculty re-flipping them with the query selctor model presented
+
+Idea: refactor functions to run with unit tests to gain deeper understanding of state of DOM before trying to fix the checkForMatch logic, an alternative would be the classic document.getElementById
 
 idea: add an unmatchable card that is automatic loss if overturned
 idea: make game scalable, more cards means less chance of overturning "bomb" but more matches need to be made?
--- memory.js
+++ memory.js
@@ -1,59 +1,25 @@
-'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-    //all that goes after is once the DOM is loaded
-    //array of 12 penguin cards with names and image filepaths
-    //question, do duplicates REALLY need to be hard-coded in this array?
-    const cards = [
+    //card options
+    var cardArray = [
         {
             name: 'penguin by water',
             img: 'Images/penguin_by_water.jpg'
         },
-
-        {
-            name: 'penguin by water',
-            img: 'Images/penguin_by_water.jpg'
-        },
-
         {
             name: 'penguin cannibal',
             img: 'Images/penguin_cannibal.jpg'
         },
         {
-            name: 'penguin cannibal',
-            img: 'Images/penguin_cannibal.jpg'
-        },
-
-        {
             name: 'penguin on rocks',
             img: 'Images/penguin_on_rocks.jpg'
         },
-
-        {
-            name: 'penguin on rocks',
-            img: 'Images/penguin_on_rocks.jpg'
-        },
-
-        {
-            name: 'penguin under water',
-            img: 'Images/penugin_under_water.jpg'
-        },
-        {
-            name: 'penguin under water',
-            img: 'Images/penugin_under_water.jpg'
-        },
-
-        {
-            name: 'penguin sign',
-            img: 'Images/penguin_sign.jpg'
-        },
-
         {
             name: 'penguin sign',
             img: 'Images/penguin_sign.jpg'
         },
         {
-            name: 'penguin wide angle',
-            img: 'Images/penguin_wide_angle.jpg'
+            name: 'penguin under water',
+            img: 'Images/penguin_under_water.jpg'
         },
         {
             name: 'penguin wide angle',
@@ -61,15 +27,69 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
-    const grid = document.querySelector('.grid');
-    //creating the board
-    function createBoard(){
-        for (let i=0;i< cards.length; i++){
-            var card = document.createElement('img')
+   // cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
+
+    function doubleArray(){
+        const arrayCopy = cardArray.map(x => x);
+        cardArray = cardArray.concat(arrayCopy);
+    }
+    //create your board
+    function createBoard() {
+        doubleArray()
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img')
             card.setAttribute('src', 'Images/card_back.jpg')
-            card.setAttribute('data-id', i);
-           // card.addEventListener('click', flipCard)
-           grid.appendChild(card)
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    //check for matches
+    function checkForMatch() {
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        if (optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'Images/card_back.jpg')
+            cards[optionTwoId].setAttribute('src', 'Images/card_back.jpg')
+            alert('You have clicked the same image!')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'Images/white.jpg')
+            cards[optionTwoId].setAttribute('src', 'Images/white.jpg')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'Images/card_back.jpg')
+            cards[optionTwoId].setAttribute('src', 'Images/card_back.jpg')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length / 2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+    }
+
+    //flip your card
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
         }
     }
 
